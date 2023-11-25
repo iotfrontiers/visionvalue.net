558c4c5b232f42b6242ae608601cea27c2dac51d
hwieonkim <hwieonkim@gmail.com>
2023-11-25 22:30:11 +0900

    서브 헤더 수정

--- components/common/page-header.vue
+++ components/common/page-header.vue
@@ -1,11 +1,14 @@
 <template>
-  <VContainer :fluid="true">
+  <VContainer class="sub-page-container" :fluid="true">
     <VRow class="page-header-row" :style="{ background: `url(/img/${props.headerBgImg})` }">
       <MainHeader />
       <CommonPageTabs style="position: absolute; left: 0; right: 0; bottom: 0" :items="props.tabItems" />
     </VRow>
-    <VRow style="min-height: 100px">
-      <slot />
+    <VRow class="content-wrap">
+      <div class="sub-title">타이틀</div>
+      <div class="section">
+        <slot />
+      </div>
     </VRow>
   </VContainer>
 </template>
@@ -28,5 +31,22 @@ const props = withDefaults(
   min-height: 1018px;
   background-size: cover;
   position: relative;
+}
+
+.sub-page-container {
+  .sub-title {
+  }
+
+  .content-wrap {
+    height: auto;
+    padding-bottom: 100px;
+    padding-top: 90px;
+    background-color: #fff;
+    color: black;
+  }
+
+  .section {
+    margin: 0 auto;
+  }
 }
 </style>
