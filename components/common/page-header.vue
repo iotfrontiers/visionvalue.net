09248a7551dbdcbf025c264040a62114891a87cc
hwieonkim <hwieonkim@gmail.com>
2023-11-25 21:15:19 +0900

    페이지 헤더 추가

---
+++ components/common/page-header.vue
@@ -0,0 +1,20 @@
+<template>
+  <VContainer>
+    <VRow class="page-header-row" :style="{ background: `url(/img/${props.headerBgImg})` }">
+      <MainHeader />
+    </VRow>
+    <VRow>
+      <slot />
+    </VRow>
+  </VContainer>
+</template>
+<script lang="ts" setup>
+const props = withDefaults(
+  defineProps<{
+    headerBgImg?: string
+  }>(),
+  {
+    headerBgImg: 'banner-biz.png',
+  },
+)
+</script>
