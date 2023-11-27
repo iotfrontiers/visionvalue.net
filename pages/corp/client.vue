f6a593991cf60af5f5a2133631cf7e27d1cb8d05
hwieonkim <hwieonkim@gmail.com>
2023-11-27 16:33:09 +0900

    주요 고객사 추가

--- pages/corp/client.vue
+++ pages/corp/client.vue
@@ -1,3 +1,51 @@
 <template>
-  <div></div>
+  <ul class="client-container">
+    <li v-for="i of 25">
+      <img :src="`/client/part_img${i}.jpg`" />
+    </li>
+  </ul>
 </template>
+<style lang="scss">
+.client-container {
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+
+  li {
+    position: relative;
+    width: 300px;
+    border: 1px solid #ddd;
+    margin: 10px 20px 10px 0;
+    list-style: none;
+
+    &::before {
+      content: '';
+      position: absolute;
+      left: 0;
+      width: 100%;
+      top: 0;
+      height: 100%;
+      border: 3px solid #007ac3;
+      transition: 0.3s;
+      opacity: 0;
+    }
+
+    // &:nth-child(4n) {
+    //   margin-right: 0;
+    // }
+
+    // &:nth-child(n + 5) {
+    //   margin-top: 22px;
+    // }
+
+    ::before {
+      box-sizing: border-box;
+    }
+
+    img {
+      border: 0;
+      max-width: 100%;
+    }
+  }
+}
+</style>
