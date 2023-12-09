42a271d2bc16610b1ef7660815612cd978980427
Roy Lee <keymasroy@gmail.com>
2023-12-09 23:30:09 +0900

    useLogger 제거

--- server/plugins/fetches.ts
+++ server/plugins/fetches.ts
@@ -1,13 +1,10 @@
-import { useLogger } from '@nuxt/kit'
 import axios from 'axios'
 
 export default defineNitroPlugin(async () => {
-  const logger = useLogger()
-
   function callApi() {
-    logger.info('fetch portfolio started....')
+    console.info('fetch portfolio started....')
     axios.post('http://localhost:12005/api/portfolio-list', {}).then(() => {
-      logger.info('fetch portfolio finished....')
+      console.info('fetch portfolio finished....')
     })
   }
 
