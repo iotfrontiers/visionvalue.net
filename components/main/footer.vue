3296f4e8d2e0ea81449ce3d4cc1538c86e117bb7
ykpark1126 <62925472+ykpark1126@users.noreply.github.com>
2024-09-06 13:41:40 +0900

    비젼밸류

--- components/main/footer.vue
+++ components/main/footer.vue
@@ -38,8 +38,8 @@
 
       FRONTIER <span class="ml-1 mr-1">|</span> 대표전화 : <a href="tel:010-7607-4451">010-7607-4451</a> <span class="ml-1 mr-1">|</span> 팩스 :
       <a href="fax:0504-163-4451">0504-163-4451</a> <span class="ml-1 mr-1">|</span> 이메일 :
-      <a href="mailto:jongju0920@kakao.com">jongju0920@kakao.com</a> <br />
-      주소 : 경기 성남시 분당구 성남대로331번길 8 9층 901호
+      <a href="mailto:ykp@visionvalue.net">ykp@visionvalue.net</a> <br />
+      주소 : 경기 성남시 분당구 성남대로331번길 8 20층 9호
     </div>
   </VFooter>
 </template>
